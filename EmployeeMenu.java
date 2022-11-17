89ecd0e19649c05537d364ad3faf89f79a8ca2ec
bozhestven <117731772+bozhestven@users.noreply.github.com>
2022-11-18 01:51:58 +0600

    Add files via upload

---
+++ EmployeeMenu.java
@@ -0,0 +1,23 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class EmployeeMenu {
+    public static void start(Employee employee) {
+    Scanner in = new Scanner(System.in);
+    while (true) {
+        System.out.println("1) Вывод информации о сотруднике");
+        System.out.println("0) Выход из главного меню");
+        int command = in.nextInt();
+        if (command == 1) {
+            System.out.println(employee);
+        } else if (command == 0) {
+            break;
+        } else {
+            System.out.println("Ошибка! Введите пункт из меню");
+        }
+    }
+}
+}
+
+
